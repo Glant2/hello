07aebc9f87e4bf50d372d38f57f3138b9aa052e0
Gavin Lant <glant2@huskers.unl.edu>
2021-08-31 01:21:07 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Gavin Lant
+ * Date: 2021/08/24
+ *
+ * A simple hello world program in c
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+     printf("Hello World!\n");
+
+     return 0;
+ }
+ 
